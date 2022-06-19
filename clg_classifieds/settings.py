d311f734dcc21786c019ac541bf985598f7b5884
Manav-arora-87 <aroramanav887@gmail.com>
2022-06-19 10:42:21 +0530

    'temp'

--- clg_classifieds/settings.py
+++ clg_classifieds/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -55,7 +55,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'clg_classifieds.urls'
-
+TEMPLATES_PATH = os.path.join(SETTINGS_PATH, 'Templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',

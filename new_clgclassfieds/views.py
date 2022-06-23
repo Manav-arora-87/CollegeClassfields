fc1c9148c0fb5cf882e0c71f1eb6b77c339724e3
Manav-arora-87 <aroramanav887@gmail.com>
2022-06-24 00:02:37 +0530

    'print'

--- new_clgclassfieds/views.py
+++ new_clgclassfieds/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.shortcuts import render
 from django.shortcuts import redirect
-
+from students.models import *
 from datetime import *
 
 # Create your views here.
 def HOME(request):
+    res=Students.objects.all()
+    print(res)
     return render(request,'index.html')
